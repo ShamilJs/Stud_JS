8e04a27b67baf3c75141b759880695e839682b7b
Shamil Samusenko <Shamilllaa@mail.ru>
2020-06-04 17:04:02 +0700

    4 commit

--- script/script.js
+++ script/script.js
@@ -1,2 +1,4 @@
-let money, income, addExpenses, deposit, mission, period;
+let money = 25;
+let income, addExpenses, deposit, mission, period;
 alert('Привет мир');
+console.log(`Проезд стоит ${money}` рублей);
